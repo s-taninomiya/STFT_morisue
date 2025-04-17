0e7c49ceeeb2f7d13641c1080aaa0834142d894e
y-morisue <yuyu1213yu@icloud.com>
2025-04-17 20:37:21 +0900

    Update main.m

--- main.m
+++ main.m
@@ -5,19 +5,33 @@ voiceData = 'parukia.wav';
 [inputSignal, fs] = audioread(voiceData);
 % sound(inputSignal, fs);
 
-% 
+% 窓長・シフト長を定義
 windowLength = 4096;
 shiftLength = windowLength / 2;
-signalLength = size(inputSignal, 1);
-complementedInputSignal = padarray(inputSignal, windowLength / 2, 0, "pre");
-complementedInputSignal = padarray(complementedInputSignal, windowLength - 1, 0, "post");
-timeFrames = ceil((2 * signalLength - windowLength) / (2 * shiftLength)) + 1;
-separatedSignals = zeros(windowLength, timeFrames);
 
-% 
+% ゼロパディング
+complementedInputSignal = padarray(inputSignal, windowLength / 2, 0, "pre");
+complementedInputSignal = padarray(complementedInputSignal, shiftLength - 1, 0, "post");
+
+% フレーム数の算出
+signalLength = size(inputSignal, 1);
+timeFrames = ceil((2 * signalLength - windowLength) / (2 * shiftLength)) + 1;
+
+% 信号を入れる行列を定義
+separatedSignals = zeros(windowLength, timeFrames);
+windowedSignals = zeros(windowLength, timeFrames);
+transformedSignals = zeros(windowLength, timeFrames);
+
+% 行列に信号を順番に代入し，ハン窓を掛ける
 for i = 1 : timeFrames
     for j = 1 : windowLength
-        separatedSignals(j, i) = complementedInputSignal((i - 1) * windowLength + j);
+        separatedSignals(j, i) = complementedInputSignal((i - 1) * shiftLength + j);
+        hannWindow = 0.5 - 0.5 * cos(2 * pi * j / windowLength);
+        windowedSignals(j, i) = separatedSignals(j, i) * hannWindow;
     end
 end
+
+% ハン窓を掛けた信号の行列をフーリエ変換する
+transformedSignals = fft(windowedSignals);
+
 
