2300776620f85b1c820eef94d6d0bb8eee35e08d
y-morisue <yuyu1213yu@icloud.com>
2025-04-22 17:36:48 +0900

    step2修正
    ステップ3は全然進んでないので無視

--- main.m
+++ main.m
@@ -18,24 +18,28 @@ signalLength = size(inputSignal, 1);
 timeFrames = ceil((2 * signalLength - windowLength) / (2 * shiftLength)) + 1;
 
 % 信号を入れる行列を定義
-separatedSignals = zeros(windowLength, timeFrames);
-windowedSignals = zeros(windowLength, timeFrames);
 transformedSignals = zeros(windowLength, timeFrames);
 
 % ハン窓の作成
 hannWindowAxis = (1:windowLength).';
 hannWindow = 0.5 - 0.5 * cos(2 * pi * hannWindowAxis / windowLength);
 
-% 行列に信号を列単位(ベクトル)で代入し，ハン窓を掛ける
+% 信号を列単位(ベクトル)でハン窓を掛ける
 for i = 1 : timeFrames
-    separatedSignals(:, i) = complementedInputSignal((i - 1) * shiftLength + 1: (i - 1) * shiftLength + windowLength, 1);
-    windowedSignals(:, i) = separatedSignals(:, i) .* hannWindow;
-    % ハン窓を掛けた後に各列をフーリエ変換をする
-    transformedSignals(:, i) = fft(windowedSignals(:, i));
+    separatedSignals = complementedInputSignal((i - 1) * shiftLength + 1: (i - 1) * shiftLength + windowLength, 1);
+    windowedSignals = separatedSignals .* hannWindow;
+    % ハン窓を掛けた後に各列をフーリエ変換して行列に挿入する
+    transformedSignals(:, i) = fft(windowedSignals);
 end
 
+% フーリエ変換後の信号を利得に変換
 poweredSignals = power(abs(transformedSignals), 2);
+signalsGain = 10 * log10(poweredSignals);
 
-
-
+time = size(complementedInputSignal, 1) / fs;
+imagesc(time, fs, signalsGain);
+xlabel("Time [s]");
+ylabel("Frequency [Hz]");
+c = colorbar;
+c.Label.String = ("Gain [dB]");
 
